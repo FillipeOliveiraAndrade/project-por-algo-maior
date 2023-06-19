0c1ecc40bfc7f34152539eb446a597f5b175dcd6
FillipeOliveiraAndrade <fillipe.trybe@gmail.com>
2023-06-19 17:20:37 -0300

    feat: Adicionando logo do youtube no footer da página

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+
 import logo from '../../assets/logoPorAlgoMaior.png'
 import logoInstagram from '../../assets/logoInstagram.png'
 import logoSpotify from '../../assets/logoSpotify.png'
+import logoYoutube from '../../assets/logoYoutube.png'
 
 import styles from './footer.module.css'
 
 function Footer() {
   const instagram = 'https://www.instagram.com/poralgomaior.ibpni/';
   const spotify = 'https://open.spotify.com/playlist/2iVqZ69P4cYox0ukgDlrLA?si=GFXClH7KRtSn5F10uwbcog';
+  const youtube = 'https://www.youtube.com/playlist?list=PLKmLY247buE_JCqxfZJA9fsET8CBaQENO';
 
   return (
     <footer className={ styles.footer__container }>
@@ -30,12 +33,21 @@ function Footer() {
           />
         </a>
 
-        { /* Instagram */ }
+        { /* Spotify */ }
         <a href={ spotify }>
           <img
             className={ styles.footer__spotify }
             src={ logoSpotify }
             alt="Spotify dos jovens"
+          />
+        </a>
+
+        { /* Youtube */ }
+        <a href={ youtube }>
+          <img
+            className={ styles.footer__youtube }
+            src={ logoYoutube }
+            alt="Youtube dos jovens"
           />
         </a>
       </div>
